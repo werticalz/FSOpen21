943fd5e91738ded7bc1b221c445ba76ee7c6c7ee
V-V Korpela <werticalz@gmail.com>
2021-10-14 17:51:42 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,28 @@
-const dummy = (blogs) => {
+const blog = require("../models/blog")
+
+var dummy = (blogs) => {
     return 1
 }
 
+var totalLikes = (blogs) => {
+
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce((sum, blog) => {
+            return sum + blog.likes
+        }, 0)
+}
+
+var favoriteBlog = (blogs) => {
+    return blogs.length === 0
+    ? "No blogs in the array"
+    : blogs.reduce((best, blog) => {
+        return best.likes > blog.likes ? best : blog
+    }, {})
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
