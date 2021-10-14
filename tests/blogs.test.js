3dfa2757729bccc30ac2df73e723fe1d94271661
V-V Korpela <werticalz@gmail.com>
2021-10-14 17:40:38 +0300

    4.4

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,8 +1,45 @@
 const dummy = require('../utils/list_helper').dummy
+const totalLikes = require('../utils/list_helper').totalLikes
+const blogs = [
+    {
+        "title": "Otsikko",
+        "author": "Tekijä",
+        "url": "osoite",
+        "likes": 2
+    },
+    {
+        "title": "Title",
+        "author": "Author",
+        "url": "URL",
+        "likes": 5
+    }
+]
+const blog = [
+    {
+        "title": "Blog",
+        "author": "blogger",
+        "url": "website",
+        "likes": 3
+    }
+]
 
-describe('Dummy', () => {
-    const blogs = []
-    test('should be one', () => {
-        expect(dummy(blogs)).toBe(1)
+
+test('Dummy returns one', () => {
+    expect(dummy(blogs)).toBe(1)
+})
+
+
+
+describe('Total likes', () => {
+    test('of an empty list returns 0', () => {
+        expect(totalLikes([])).toBe(0)
+    })
+
+    test('of a list of only one blog to be right', () => {
+        expect(totalLikes(blog)).toBe(3)
+    })
+
+    test('of a list of multiple blogs', () => {
+        expect(totalLikes(blogs)).toBe(7)
     })
 })
