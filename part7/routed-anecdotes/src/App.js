42899439be5738d71b16018d3f0a65ab5e2c5b77
V-V Korpela <werticalz@gmail.com>
2021-11-14 21:32:29 +0200

    7.1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -45,7 +45,7 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
 
-    See <a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a href='https://github.com/werticalz/FSOpen21/blob/part7/part7/routed-anecdotes/src/App.js'>https://github.com/werticalz/FSOpen21/blob/part7/part7/routed-anecdotes/src/App.js</a> for the source code.
   </div>
 )
 
