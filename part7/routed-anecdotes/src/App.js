7e5f8d3d3304ccab6bc07c54739a6dad8f9fee64
V-V Korpela <werticalz@gmail.com>
2021-11-18 19:54:26 +0200

    7.2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {
+  useParams,
   BrowserRouter as Router,
   Switch, Route, Link
 } from 'react-router-dom'
@@ -18,11 +19,27 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(a => Number(id) === Number(a.id))
+
+  return (
+    <div>
+      <h3>{anecdote.content}</h3>
+      <div>By {anecdote.author}</div>
+      <div>Has {anecdote.votes} votes</div>
+      <div>for more information see <a href={anecdote.info}>{anecdote.info}</a></div>
+    </div>
+  )
+}
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}>
+        <Link to={`/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
@@ -53,7 +70,6 @@ const CreateNew = (props) => {
   const [content, setContent] = useState('')
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -132,8 +148,11 @@ const App = () => {
       <div>
         <Menu />
         <Switch>
+          <Route path='/:id'>
+            <Anecdote anecdotes={anecdotes} />
+          </Route>
           <Route path='/create'>
-            <CreateNew />
+            <CreateNew anecdotes={anecdotes} addNew={addNew} />
           </Route>
           <Route path='/about'>
             <About />
