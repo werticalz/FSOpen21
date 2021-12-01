47fc3db5847cb8d2a0abb1d7648f5016a72cd121
V-V Korpela <werticalz@gmail.com>
2021-12-01 18:17:10 +0200

    7.7

---
+++ part7/country-hook/src/components/Country.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+  if (country.status === 404) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country[0].name} </h3>
+      <div>capital {country[0].capital} </div>
+      <div>population {country[0].population}</div>
+      <img src={country[0].flag} height='100' alt={`flag of ${country[0].name}`} />
+    </div>
+  )
+}
+
+export default Country
